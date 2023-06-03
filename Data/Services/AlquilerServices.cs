492d8e65249b1dd3c4e9da123127e693b07e57ad
Pedro Nuñez <pedroantonionunezheredia3@gmail.com>
2023-06-03 16:24:16 -0400

    Servicios Extras

--- Data/Services/AlquilerServices.cs
+++ Data/Services/AlquilerServices.cs
@@ -1,7 +1,16 @@
-﻿namespace Nuñez_Inmobiliaria.Data.Services
+﻿using Nuñez_Inmobiliaria.Data.Context;
+
+namespace Nuñez_Inmobiliaria.Data.Services
 {
     public class AlquilerServices
     {
+        private readonly INunezInmobiliariaDbContext dbContext;
 
+        public AlquilerServices(INunezInmobiliariaDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        
     }
 }
