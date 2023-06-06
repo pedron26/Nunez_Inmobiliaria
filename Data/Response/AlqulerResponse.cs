81b3779c7db21d9a60312ff987faad2dc673c0eb
pedron26 <132863262+pedron26@users.noreply.github.com>
2023-06-06 17:19:58 -0600

    Entidad del Proyecto

--- Data/Response/AlqulerResponse.cs
+++ Data/Response/AlqulerResponse.cs
@@ -14,6 +14,24 @@
         public InmuebleResponse? Inmueble { get; set; }
         public TipoPagoResponse? TipoPago { get; set; }
         public ICollection<PagoResponse>? Pagos { get; set; }
+
+
+
+        public AlquilerRequest ToRequest()
+        {
+            return new AlquilerRequest
+            {
+               Id = Id,
+               ClienteId = ClienteId,
+               InmuebleId = InmuebleId,
+               TipoPagoId = TipoPagoId,
+               Fecha = Fecha,
+               FechaInicio = FechaInicio,
+               FechaCulminacion = FechaCulminacion,
+               FechaDePago  = FechaDePago,
+
+            };
+        }
     }
 
 }
